230fbb2b0a53aedd7ba0d60c870cd547b9468dd4
Jim Bosch <jbosch@astro.princeton.edu>
2012-07-29 14:07:13 +0900

    switch from super() to explicit base class for clarity, fix error message

--- python/hsc/pipe/base/argumentParser.py
+++ python/hsc/pipe/base/argumentParser.py
@@ -3,16 +3,16 @@ from lsst.pipe.base import ArgumentParser
 
 class SubaruArgumentParser(ArgumentParser):
     def __init__(self, *args, **kwargs):
-        super(SubaruArgumentParser, self).__init__(*args, **kwargs)
+        ArgumentParser.__init__(self, *args, **kwargs)
         self.add_argument('--rerun', type=str, default=None, help='Desired rerun (overrides --output)',
                           action="store", dest="rerun")
 
     def _fixPaths(self, namespace):
         if namespace.rerun and namespace.output:
             argparse.ArgumentTypeError("Please specify --output or --rerun, but not both")
-        super(SubaruArgumentParser, self)._fixPaths(namespace)
+        ArgumentParser._fixPaths(self, namespace)
         if namespace.rerun:
             root = os.environ.get("SUPRIME_DATA_DIR")
             if not root:
-                argparse.ArgumentTypeError("Please specify --output or --rerun, but not both")
+                argparse.ArgumentTypeError("Cannot use --rerun without setting up suprime_data")
             namespace.output = os.path.join(root, "rerun", namespace.rerun)
