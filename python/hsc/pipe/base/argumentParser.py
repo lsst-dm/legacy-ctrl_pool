40034c4aa7d2ccec9ee32b4feedce8499a7eed8c
Jim Bosch <jbosch@astro.princeton.edu>
2012-07-31 22:23:46 +0900

    refine rerun handling so we can use it with processing stacks

--- python/hsc/pipe/base/argumentParser.py
+++ python/hsc/pipe/base/argumentParser.py
@@ -3,7 +3,9 @@ from .camera import parseInstrument
 from lsst.pipe.base import ArgumentParser
 
 class SubaruArgumentParser(ArgumentParser):
+
     def __init__(self, *args, **kwargs):
+        self.defaultToUserName = kwargs.pop("defaultToUserName", True)
         ArgumentParser.__init__(self, *args, **kwargs)
         self.add_argument('--rerun', type=str, default=None, help='Desired rerun (overrides --output)',
                           action="store", dest="rerun")
@@ -11,7 +13,7 @@ class SubaruArgumentParser(ArgumentParser):
     def _fixPaths(self, namespace):
         if namespace.rerun and namespace.output:
             argparse.ArgumentTypeError("Please specify --output or --rerun, but not both")
-        if namespace.rerun is None and namespace.output is None:
+        if self.defaultToUserName and namespace.rerun is None and namespace.output is None:
             namespace.rerun = getpass.getuser()
         ArgumentParser._fixPaths(self, namespace)
         if namespace.rerun:
