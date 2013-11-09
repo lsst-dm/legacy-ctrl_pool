63c6ae3f6db3deff6bbcb38ad87c051fbfa99124
Paul Price <price@astro.princeton.edu>
2013-11-09 00:16:21 -0500

    log: easier method of pickling a Log
    Subclassing Log in python was problematic because C++ classes
    wanted access to it.  Instead, pickle a Log by assuming that
    we're always using the default log and never modifying it.

--- python/hsc/pipe/base/log.py
+++ python/hsc/pipe/base/log.py
@@ -1,51 +1,24 @@
-import lsst.pex.logging
+import os
+import copy_reg
 
-__all__ = ["PicklableLog", "getDefaultLog"]
+import lsst.pex.logging as pexLog
 
-OriginalLog = lsst.pex.logging.Log
-defaultLog = lsst.pex.logging.getDefaultLog()
+def pickleLog(log):
+    """Pickle a log
 
-class PicklableLog(object):
-    """Log that can be pickled
-
-    Wraps the LSST default log object and reinstantiates
-    it on the other side, re-adding all the destinations.
-    Assumes:
-    1. The only log in use is the default log.
-    2. The only actions on the log beside logging that change
-       the log state are adding output destinations.
+    Drop the log on the floor; we'll get a new default.
+    Assumes that we're always just using the default log,
+    and not modifying it.
     """
-    def __init__(self, log=None, name=None, destination=[]):
-        """Constructor
+    return pexLog.getDefaultLog, tuple()
 
-        @param log: Log to wrap; None for LSST default log
-        @param name: Name to give log, or None
-        @param destination: List of log files to write
-        """
-        if log is not None:
-            if isinstance(log, PicklableLog):
-                destination.extend(log._destination)
-                log = log._log
-        else:
-            log = defaultLog
-        if name is not None:
-            log = OriginalLog(log, name)
-        self._log = log
-        self._name = name
-        self._destination = destination
-        for dest in destination:
-            self._log.addDestination(dest)
-    def __getattr__(self, name):
-        return getattr(self._log, name)
-    def __reduce__(self):
-        return self.__class__, (None, self._name, self._destination)
-    def addDestination(self, dest):
-        self._destination.append(dest)
-        self._log.addDestination(dest)
+copy_reg.pickle(pexLog.Log, pickleLog)
+copy_reg.pickle(pexLog.ScreenLog, pickleLog)
 
-def getDefaultLog():
-    return PicklableLog()
+def jobLog(job):
+    """Add a job-specific log destination"""
+    if job is None or job != "None":
+        return
+    machine = os.uname()[1].split(".")[0]
+    pexLog.getDefaultLog().addDestination(job + ".%s.%d" % (machine, os.getpid()))
 
-defaultLog.warn("Monkey-patching lsst.pex.logging with PicklableLog")
-lsst.pex.logging.getDefaultLog = getDefaultLog
-lsst.pex.logging.Log = PicklableLog
